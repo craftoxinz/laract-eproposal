e1785c239a54d8ddde8d60dda69c4afe0a318f1f
craftoxinz <winnestkyouka12@gmail.com>
2026-07-09 11:35:58 +0700

    feat(database): add students table migration

---
+++ database/migrations/2026_07_08_061046_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->string('student_number')->unique()->nullable(); // nim
+            $table->foreignId('study_program_id')->nullable()->constrained();
+            $table->unsignedSmallInteger('academic_year')->nullable();
+            $table->string('phone_number')->unique()->nullable();
+            $table->string('photo_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
