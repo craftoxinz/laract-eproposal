2c5f2211372df8faa2f910675434b41fe63bd444
craftoxinz <winnestkyouka12@gmail.com>
2026-07-10 00:29:20 +0700

    feat(database): add lecturers table migration

---
+++ database/migrations/2026_07_09_171131_create_lecturers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->string('name')->nullable();
+            $table->string('lecturer_number')->unique()->nullable(); // nip
+            $table->foreignId('study_program_id')->nullable()->constrained();
+            $table->string('phone_number')->unique()->nullable();
+            $table->string('photo_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lecturers');
+    }
+};
