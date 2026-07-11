6b803b9319a81b8bef3f6232b77c0760ac34a56f
craftoxinz <winnestkyouka12@gmail.com>
2026-07-11 22:06:06 +0700

    feat(routes): add student dashboard and update-profile web routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    Route::prefix('students')->name('students.')->group(function () {
+        Route::inertia('dashboard', 'student/dashboard')->name('dashboard');
+        Route::inertia('update-profile', 'student/update-profile')->name('update-profile');
+    });
 });
 
 require __DIR__.'/settings.php';
